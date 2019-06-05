fc814b5e5812b09563cca413d966c3959e7bfd73
Brian Pratt <brian@pratt.io>
2019-06-05 09:42:46 -0500

    TUESDAY

--- src/coffee/HardwareAPI.ts
+++ src/coffee/HardwareAPI.ts
@@ -8,18 +8,6 @@ export default interface HardwareAPI {
     setValveState(state: number): void;
 }
 
-// [CoffeeMaker] -> [HardwareAdapter] <- [HardwareAPI]
-// only mock what you own
-
-// ISP
-// the interface-segregation principle (ISP) states that no client 
-// should be forced to depend on methods it does not use.[1] 
-// ISP splits interfaces that are very large into smaller and more 
-// specific ones so that clients will only have to know about the methods
-// that are of interest to them. Such shrunken interfaces are also called role
-// interfaces.[2] ISP is intended to keep a system decoupled and thus easier to refactor, 
-// change, and redeploy
-
 // MK II: tank/reservoir
 // MK III: wall
 // MK IV: vending machine / lattes
--- test/Sandbox.test.ts
+++ test/Sandbox.test.ts
@@ -1,23 +1,49 @@
-// each
-//  - pass it a list
-//  - execute a block of code for every value on this list
-//  each([1, 2], (n) => blah(n))
+import { Identifier } from "@babel/types";
 
-function each(
-  list: number[], 
-  block: (n: number) => void
-): void {
 
+interface CreatePostParams { 
+  author: Author, 
+  title: string, 
+  body: string 
 }
 
-describe('each', () => {
-  it('executes the given fn for every value on the list', () => {
-    const list = [1, 2];
-    let results = [];
+class Author {
+  readonly id: number;
+  readonly name: string;
 
-    each(list, (n) => results.push(n));
-    
-    expect(results).toEqual([1, 2]);
+  constructor(params: { id: number, name: string }) {
+    this.id = params.id;
+    this.name = params.name;
+  }
+}
+class BlogEngine {
+  createPost(params: CreatePostParams) {
+    return new Post(params);
+  }
+}
+
+class Post {
+  readonly authorId: number;
+  readonly title: string;
+  readonly body: string;
+  readonly id: number;
+
+  constructor(params: CreatePostParams) {
+    this.authorId = params.author.id;
+    this.title = params.title;
+    this.body = params.body;
+  }
+}
+
+describe('BlogEngine', () => {
+  it('creates a new post', ()  => {
+    let engine = new BlogEngine();
+    let author = new Author({ id: 1, name: 'Brian' });
+  
+    let post = engine.createPost({
+      author: author,
+      title: 'my cool post',
+      body: 'its a new blog and its real nice'
+    });
   });
-
 });
